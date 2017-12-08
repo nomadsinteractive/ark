5fe1caaf8d5bc22902fa89f1455b60370900799f
nomadsinteractive <nomadsinteractive@gmail.com>
2017-12-08 18:41:17 +0800

    alphabet loading fallback strategy

--- src/graphics/impl/alphabet/alphabet_with_fallback.h
+++ src/graphics/impl/alphabet/alphabet_with_fallback.h
@@ -15,6 +15,18 @@ public:
     virtual bool load(uint32_t c, uint32_t& width, uint32_t& height, bool loadGlyph, bool hasFallback) override;
     virtual void draw(const bitmap& image, int32_t x, int32_t y) override;
 
+//  [[plugin::style("fallback")]]
+    class STYLE : public Builder<Alphabet> {
+    public:
+        STYLE(BeanFactory& factory, const sp<Builder<Alphabet>>& delegate, const String& value);
+
+        virtual sp<Alphabet> build(const sp<Scope>& args) override;
+
+    private:
+        sp<Builder<Alphabet>> _delegate;
+        sp<Builder<Alphabet>> _fallback;
+    };
+
 private:
     sp<Alphabet> _alphabet;
     sp<Alphabet> _fallback;
