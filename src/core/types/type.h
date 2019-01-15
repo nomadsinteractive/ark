bd25acba9b37cb263fefee35bdd6ae94b3c09a02
nomadsinteractive <nomadsinteractive@gmail.com>
2019-01-15 23:56:39 +0800

    fix osx compiler error

--- src/core/types/type.h
+++ src/core/types/type.h
@@ -5,10 +5,6 @@
 
 namespace ark {
 
-inline _CONSTEXPR uint32_t _hash(const char* s) {
-    return static_cast<uint32_t>(*s ? (*(s + 1) ? *s + (*(s + 1)) * 101ull + _hash(s + 2) * 101ull * 101ull : *s) : 0);
-}
-
 template<typename T> class Type {
 public:
     static _CONSTEXPR TypeId id() {
@@ -23,6 +19,12 @@ public:
 private:
     static _CONSTEXPR TypeId _id() {
         return _hash(__ARK_FUNCTION__);
+    }
+
+    static _CONSTEXPR uint32_t _hash(const char* s) {
+        return static_cast<uint32_t>(*s ? (*(s + 1) ? *s + (*(s + 1)) * 101ull +
+                                                      (*(s + 2) ? (*(s + 3) ? *(s + 2) + (*(s + 3)) * 101ull + _hash(s + 4) * 101ull * 101ull : *(s + 2)) : 0)
+                                                      * 101ull * 101ull : *s) : 0);
     }
 
 #ifdef ARK_USE_CONSTEXPR
