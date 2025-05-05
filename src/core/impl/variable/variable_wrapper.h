bbb6c0e3ddb97b31ca050c9e9caaf5398e458b2c
nomadsinteractive <nomadsinteractive@gmail.com>
2025-05-05 13:27:33 -0700

    remove wrong noexcept modifier

--- src/core/impl/variable/variable_wrapper.h
+++ src/core/impl/variable/variable_wrapper.h
@@ -11,14 +11,14 @@ namespace ark {
 
 template<typename T> class VariableWrapper final : public Variable<T>, public Wrapper<Variable<T>>, Implements<VariableWrapper<T>, Variable<T>, Wrapper<Variable<T>>> {
 public:
-    VariableWrapper(sp<Variable<T>> delegate) noexcept
+    VariableWrapper(sp<Variable<T>> delegate)
         : Wrapper<Variable<T>>(std::move(delegate)) {
         ASSERT(this->_wrapped);
     }
-    VariableWrapper(T value) noexcept
+    VariableWrapper(T value)
         : Wrapper<Variable<T>>(sp<Variable<T>>::template make<typename Variable<T>::Const>(value)) {
     }
-    DEFAULT_COPY_AND_ASSIGN_NOEXCEPT(VariableWrapper);
+    DEFAULT_COPY_AND_ASSIGN(VariableWrapper);
 
     T val() override {
         return this->_wrapped->val();
